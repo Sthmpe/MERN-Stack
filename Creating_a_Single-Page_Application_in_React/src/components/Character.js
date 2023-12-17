2b8d94e7120be772cbf6f978ad88872a9bde4a46
David <olaniteolanight@gmail.com>
2023-12-17 05:16:38 +0100

    Re-editing

--- Creating_a_Single-Page_Application_in_React/src/components/Character.js
+++ Creating_a_Single-Page_Application_in_React/src/components/Character.js
@@ -1,15 +1,17 @@
 function Character(character) {
 	return (
 		<div className='col-3'>
-			<div className='card'>
-				<img 
-					src={character.image} 
-					alt={character.name}
-					className='card-img-top'
-				/>
-				<div className='card-body'>
-					<h3 className='card-title'>{character.name}</h3>
-					<p>{'Origin: ${character.origin && character.origin.name}'}</p>
+			<div className='card mb-3'>
+				<div className='row g-3'>
+					<img 
+						src={character.image} 
+						alt={character.name}
+						className='img-fluid card-img-top'
+					/>
+					<div className='card-body'>
+						<h3 className='card-header'>{character.name}</h3>
+						<p>{`Origin: ${character.origin && character.origin.name}`}</p>
+					</div>
 				</div>
 			</div>
 		</div>
--- Creating_a_Single-Page_Application_in_React/src/index.js
+++ Creating_a_Single-Page_Application_in_React/src/index.js
@@ -1,12 +1,17 @@
 import ReactDOM from 'react-dom/client';
 import List from './components/list';
+import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+	const []
+	
 	return(
-		<div className='container'>
-			<h1>Rick and Morty</h1>
-			<List/>
+		<div className='container' data-bs-theme='bs-body-bg'>
+			<nav className='navbar stixk-top navbar-light bg-dark'>
+				<h1 className='navbar-brand text-light'>RICK AND MORTY</h1>
+			</nav>
+			<List />
 		</div>
 	);
 }
